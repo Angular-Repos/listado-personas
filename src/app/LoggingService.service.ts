f7e39bf8615c2ea9ff96e05bed4794ab46d79e8a
Enoc-Martinez <shanock.charras@gmail.com>
2020-03-11 10:34:31 -0600

    Using service as logging

---
+++ src/app/LoggingService.service.ts
@@ -0,0 +1,5 @@
+export class LoggingService {
+  sendConsoleMessage(message: string) {
+    console.log(message);
+  }
+}
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,12 @@
 import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { Persona } from '../Persona.model';
+import { LoggingService } from '../LoggingService.service';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
+  providers: [LoggingService]   // para poder inyectarlo, tenemos que configurarlo en los providers
 })
 export class FormularioComponent {
   @Output() personaCreada = new EventEmitter<Persona>();    // para emitir un evento de un hijo a un padre
@@ -13,10 +15,11 @@ export class FormularioComponent {
   // nombreInput: string;
   // apellidoInput: string;
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }   // para usar un service, tenemos que inyectarlo en el constructor (usando forma corta)
 
   onAgregarPersona() {
     const persona = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
+    this.loggingService.sendConsoleMessage(`Persona agregada ${JSON.stringify(persona)}`);
     this.personaCreada.emit(persona);
   }
 }
